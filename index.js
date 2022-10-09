dfa627a76ea0b5a849280cafdde7e8841c9e0fec
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2022-10-10 01:18:42 +0600

    Added Cors and  added origin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const cokieParser = require("cookie-parser");
 const userRoutes = require("./routes/userRoutes");
 const bookRoutes = require("./routes/bookRoutes");
@@ -8,6 +9,13 @@ const bookRoutes = require("./routes/bookRoutes");
 dotenv.config();
 
 const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cokieParser());
