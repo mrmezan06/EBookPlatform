00e44ca04b510ea7f00989c19e0d04cf830a1232
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2022-10-12 02:33:20 +0600

    Full Responsiveness added

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,12 +4,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse, faSearch } from "@fortawesome/free-solid-svg-icons";
 import "./navbar.css";
 import UserMenu from "../userMenu/UserMenu";
+import { useEffect } from "react";
 
 const Navbar = () => {
+  const [showCategories, setShowCategories] = React.useState(true);
+
   const navigate = useNavigate();
   const handleSearch = () => {
     navigate("/search");
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setShowCategories(false);
+      } else {
+        setShowCategories(true);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+  }, [showCategories]);
+
   return (
     <div className="navContainer">
       <div className="logo">
@@ -19,18 +34,22 @@ const Navbar = () => {
       </div>
 
       <div className="menu">
-        <Link to="/?cat=programming" className="link">
-          Programming
-        </Link>
-        <Link to="/?cat=hacking" className="link">
-          Hacking
-        </Link>
-        <Link to="/?cat=networking" className="link">
-          Networking
-        </Link>
-        <Link to="/?cat=cybersecurity" className="link">
-          Security
-        </Link>
+        {showCategories && (
+          <>
+            <Link to="/?cat=programming" className="link">
+              Programming
+            </Link>
+            <Link to="/?cat=hacking" className="link">
+              Hacking
+            </Link>
+            <Link to="/?cat=networking" className="link">
+              Networking
+            </Link>
+            <Link to="/?cat=cybersecurity" className="link">
+              Security
+            </Link>
+          </>
+        )}
 
         <div className="search">
           <FontAwesomeIcon
